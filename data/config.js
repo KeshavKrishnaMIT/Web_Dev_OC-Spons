f1a5a219e2fa5fca615cd9de943c4fa827ff5399
Keshav Krishna <keshavkrishna235@gmail.com>
2026-02-19 18:51:12 +0530

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,4 @@
+// ===== CC AUTHENTICATION KEY =====
+// This key is required when creating a Core Committee (CC) account.
+// Change this value to your own secret key.
+var CC_AUTH_KEY = "spons2026cc";
